b0de931251ba3b3905352a45a32ebbe085b07dbd
WisnuWicaksono46 <58814466+WisnuWicaksono46@users.noreply.github.com>
2024-01-24 18:06:03 +0700

    Delete penelitian_ilmiah/data_extractor directory

--- penelitian_ilmiah/data_extractor/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import extract_folder
-from .views import download_zip
-
-urlpatterns = [
-    path('', extract_folder, name='extract_folder'),
-    path('success/', download_zip, name='download_zip')
-]
