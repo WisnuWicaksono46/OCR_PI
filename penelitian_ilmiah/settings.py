b6bdf877f3de79ca2ef617e1c390c88d543d1570
WisnuWicaksono46 <58814466+WisnuWicaksono46@users.noreply.github.com>
2024-01-25 16:23:20 +0700

    Update settings.py

--- penelitian_ilmiah/settings.py
+++ penelitian_ilmiah/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*etxe_+ocwxrbdwox^0=cqskgd7*kfiap#8b!xg@eo+0+7p$&l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
